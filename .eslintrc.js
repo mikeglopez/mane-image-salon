66ba73c96bd950a8153e64c4e143925939bf0062
Mike <mikeglopez.91@gmail.com>
2019-10-05 17:37:36 -0500

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  env: {
+    'browser': 'true'
+  },
+  extends: 'airbnb',
+  parserOptions: {
+    'ecmaVersion': 2018
+  },
+  rules: {
+    'comma-dangle': 'off',
+    'import/extensions': 'off',
+    'no-console': 'off',
+    'no-plusplus': 'off'
+  }
+};
